4caae05a64033289f70fdaf510a3ff1ba065aba6
Kane Tran <kanetran29@gmail.com>
2021-11-30 22:38:26 +0700

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AthletesComponent } from './components/athletes/athletes.component';
+import { SettingComponent } from './components/setting/setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/athletes', pathMatch: 'full' },
+  { path: 'settings', component: SettingComponent },
+  { path: 'athletes', component: AthletesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { AthletesComponent } from './components/athletes/athletes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingComponent,
+    AthletesComponent
   ],
   imports: [
     BrowserModule,
